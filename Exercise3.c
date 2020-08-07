9aa67b2ab7bf7540bef98455ea1809da69ecbaf0
duccong1 <69331679+duccong1@users.noreply.github.com>
2020-08-07 12:20:52 +0700

    Update Exercise3.c

--- Exercise3.c
+++ Exercise3.c
@@ -6,18 +6,63 @@ Ex:
 | Output: "a" "smallest"                                   |
 |__________________________________________________________|
 */
+/*
+3. Input a string and find the smallest and largest word.
+Ex:
+ __________________________________________________________
+| Input: "this is a string with smallest and largest word" |
+| Output: "a" "smallest"                                   |
+|__________________________________________________________|
+*/
 #include <stdio.h>
 #include <stdlib.h>
+#include<string.h>
+int max(char x[])
+{	
+    char b[30];	
+    char c[30];		
+    int j = 0, maxlength = 0;
+    int i = 0;
 
-int ex3(char *str){
-	//your codes here
+    for(i = 0; i <= strlen(x); i++){
+        if(x[i] != 32 && x[i] != '\0'){
+	    b[j++] = x[i];
+	}else{
+            b[j] = '\0';
+            if(strlen(b) > maxlength){
+                strcpy(c, b);		// b -> c
+                maxlength = strlen(b);	// update maxLength
+            }
+            j = 0;
+        }
+    }
+    puts(c);
 }
+int min(char x[])
+{	
+    char b[30];	
+    char c[30];		
+    int j = 0, min= strlen(x);
+    int i = 0;
 
-int main(int argc, char *argv[]) {
-	//testing variable, applying it to your algorithm for auto-evaluating
-	char *str = "This is a string";
-
-	ex3(str);
-		
+    for(i = 0; i <= strlen(x); i++){
+        if(x[i] != 32 && x[i] != '\0'){
+	    b[j++] = x[i];
+	}else{
+            b[j] = '\0';
+            if(strlen(b) < min){
+                strcpy(c, b);		
+                min = strlen(b);	
+            }
+            j = 0;
+        }
+    }
+    puts(c);
+}
+int main()
+{
+	char x[]="This is a string";
+	max(x);
+	min(x);
 	return 0;
 }
